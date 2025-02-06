0ca10230fd4b4cbb06ab1b112b076f712424ad79
Aayushi Shukla <AayushiShukla27@gmail.com>
2025-02-05 22:48:06 -0500

    Added eye candy

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const container = document.getElementById("container");
+
 const start = document.getElementById("start");
 const pause = document.getElementById("pause");
 const stop = document.getElementById("stop");
@@ -9,7 +11,7 @@ const large = document.getElementById("large");
 
 const timer = document.getElementById("timer");
 
-let timeLeft = 3600;    // small session --> 1 hour
+let timeLeft = 605;    // small session --> 1 hour
 let interval;
 
 const updateTimer = () => {
@@ -39,12 +41,32 @@ large.addEventListener("click", () => {
     updateTimer();
 });
 
+// Change Background Color
+
+const updateBGColor = () => {
+    document.body.style.background = "#C41E3A";
+    //document.body.style.background = "linear-gradient(180deg, #ff8177, #b12a5b);";
+
+    // Shake the div - to convey that 10 minutes are left
+    container.classList.add("shake");
+}
+
+const resetBGColor = () => {
+    document.body.style.background = "linear-gradient(180deg, #191654, #43C6AC)";
+    container.classList.remove("shake");
+}
+
 //Set Timer Actions
 
 const startTimer = () => {
     interval = setInterval(() => {
         timeLeft--;
         updateTimer();
+
+        // Change background color when 10 minutes are left on the timer
+        if(timeLeft === 600) {
+            updateBGColor();
+        }
 
         if(timeLeft === 0) {
             clearInterval(interval);
@@ -55,10 +77,6 @@ const startTimer = () => {
     }, 1000);
 }
 
-// const pauseTimer = () => {
-    
-// }
-
 const stopTimer = () => {
     clearInterval(interval);
 }
@@ -67,6 +85,7 @@ const resetTimer = () => {
     clearInterval(interval);
     timeLeft = 3600;
     updateTimer();
+    resetBGColor();
 }
 
 start.addEventListener("click", startTimer);
