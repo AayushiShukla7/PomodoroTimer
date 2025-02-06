90fc9f86678d46cdf75910ff0bc1a2edaac7bd9b
Aayushi Shukla <AayushiShukla27@gmail.com>
2025-02-05 21:52:21 -0500

    CSS completed

---
+++ app.js
@@ -0,0 +1,8 @@
+const start = document.getElementById("start");
+const pause = document.getElementById("pause");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+const small = document.getElementById("small");
+const medium = document.getElementById("medium");
+const large = document.getElementById("large");
